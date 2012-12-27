5d2359423b7fd7b4ecefb7af6c616d06f88ab73f
fxpfxpdav <fxpfxpdav@walla.com>
2012-12-27 23:14:10 +0200

    added words to text indexer

--- text_indexer/ui/words_index_panel.py
+++ text_indexer/ui/words_index_panel.py
@@ -6,23 +6,53 @@ import  wx.grid
 class WordsIndexPanel(wx.Panel):
     
     def __init__(self, parent):
+        from text_indexer.orm.base import session
+        from text_indexer.orm.word_position import WordPosition
+        
         wx.Panel.__init__(self, parent, -1)
         
         self.grid = wx.grid.Grid(self, -1, (100, 50), (1200, 400))
         
-        self.grid.CreateGrid(100000, 4)
+        wps = session.query(WordPosition).order_by(WordPosition.song_id, WordPosition.line_number,WordPosition.row_word_number).all()
         
-        self.grid.SetColSize(0, 300)
-        self.grid.SetColSize(1, 300)
-        self.grid.SetColSize(2, 300)
-        self.grid.SetColSize(3, 300)
+        self.grid.CreateGrid(len(wps), 5)
+        
+        self.grid.SetColSize(0, 200)
+        self.grid.SetColSize(1, 200)
+        self.grid.SetColSize(2, 200)
+        self.grid.SetColSize(3, 200)
+        self.grid.SetColSize(4, 200)
         
         self.grid.SetColLabelValue(0, "Words")
-        self.grid.SetColLabelValue(1, "Number in song")
-        self.grid.SetColLabelValue(2, "Paragraph")
-        self.grid.SetColLabelValue(3, "Number in paragraph")
+        self.grid.SetColLabelValue(1, "Song")
+        self.grid.SetColLabelValue(2, "Number in song")
+        self.grid.SetColLabelValue(3, "Paragraph")
+        self.grid.SetColLabelValue(4, "Number in paragraph")
+        
+        song = wps[0].song.name
+        number=0
+        number_in_song = 1
+        paragraph=1
+        number_in_paragraph = 1
+        for w in wps:
+            if w.song.name != song:
+                song = w.song.name
+                number_in_song = 1
+                paragraph=1
+                number_in_paragraph = 1
+            if w.stanza_number > paragraph:
+                paragraph = w.stanza_number
+                number_in_paragraph = 1
+            self.grid.SetCellValue(number, 0, w.word.word)
+            self.grid.SetCellValue(number, 1, song)
+            self.grid.SetCellValue(number, 2, str(number_in_song))
+            self.grid.SetCellValue(number, 3, str(paragraph))
+            self.grid.SetCellValue(number, 4, str(number_in_paragraph))
+            number+=1
+            number_in_paragraph+=1
+            number_in_song+=1
                 
-        self.grid.SetCellValue(0, 0, "First cell")
-        self.grid.SetCellValue(1, 1, "Another cell")
-        self.grid.SetCellValue(2, 2, "Yet another cell")
-        self.grid.SetCellValue(3, 3, "This cell is read-only")
+            
+            
+                
+        
